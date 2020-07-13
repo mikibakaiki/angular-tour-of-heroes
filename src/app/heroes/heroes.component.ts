47aaf520bf8f0f5825c7eaee3338cc2a52714d8b
mikibakaiki <joaomiguelcampos6@gmail.com>
2020-07-13 09:30:47 +0100

    Converted from .css to .scss

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,7 +5,7 @@ import { HeroService } from '../services/hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = { id: 1, name: 'Windstorm' };
